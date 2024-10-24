66bbe90dd8f8fd8a70ad7c046d284493cd13356f
Raj-Mayank2 <vmayank0123@gmail.com>
2024-10-24 07:08:58 +0530

    my-second day

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -3,7 +3,7 @@ import './Cart.css'
 import { StoreContext } from '../../context/StoreContext'
 const Cart = () => {
 
-  const { cartItems, food_list, removeFromCart } = useContext(StoreContext);
+  const { cartItems, food_list, removeFromCart, getTotalCartAmount} = useContext(StoreContext);
 
   return (
     <div className='cart'>
@@ -43,17 +43,17 @@ const Cart = () => {
           <div>
             <div className="cart-total-details">
               <p>SubTotal</p>
-              <p>{0}</p>
+              <p>Rs{getTotalCartAmount()}</p>
             </div>
             <hr />
             <div className="cart-total-details">
               <p>Delivery Fee</p>
-              <p>{2}</p>
+              <p>Rs{2}</p>
             </div>
             <hr />
             <div className="cart-total-details">
               <b>Total</b>
-              <b>{0}</b>
+              <b>Rs{getTotalCartAmount()+2}</b>
             </div>
           </div>
           <button>Proceed to Checkout</button>
